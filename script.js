da7a80ab7986799e57f4d7e370890256cce7cdb5
Albin <142149906+mkdew37@users.noreply.github.com>
2024-09-12 17:47:50 +0200

    Added a operator function, using switch statement

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Variables
+let number1 = 0;
+let number2 = 0;
+let operator;
+
 //Functions
 function add(num1, num2)  {
     return num1 + num2;
@@ -13,4 +18,25 @@ function multiply(num1, num2)   {
 
 function divide(num1, num2) {
     return num1/num2;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator)   {
+
+        case '+':
+            return add(num1, num2);
+            break;
+
+        case '-':
+            return subtract(num1, num2);
+            break;
+
+        case '*':
+            return multiply(num1, num2);
+            break;
+
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
 }
